6d21fde63cf40b05613eb3a448408d2069f64c3f
Taulant Ibraj <tibraj94@gmail.com>
2020-04-14 14:05:41 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,116 @@
+class TicTacToe
+  def initialize
+    @board = Array.new(9, " ")
+  end
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2],
+    ]
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(userinput)
+  userinput.to_i - 1
+  end
+
+  def move(index, value = "X")
+    @board[index] = value
+  end
+
+  def position_taken?(index)
+    if (@board[index] == " " || @board[index] == "" || @board[index] == nil)
+      return false
+    else
+      return true
+    end
+  end
+
+  def valid_move?
+    if index.between?(0, 8) && position_taken?(board, index) == false
+      return true
+    else
+      return false
+    end
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    user_move = input_to_index(gets.strip.to_i)
+      if valid_move?(board, user_move) == true
+        move(board, user_move, current_player(board))
+        display_board(board)
+      else
+        turn(board)
+      end
+  end
+
+  def turn_count
+    counter = 0
+    board.each do |turn|
+      if turn == "X" || turn == "O"
+      counter += 1
+      end
+    end
+    counter
+  end
+
+  def current_player
+    if turn_count(board) % 2 == 0
+      "X"
+    else
+      "O"
+    end
+  end
+
+  def won?
+    WIN_COMBINATIONS.detect do |winning_com|
+      board[winning_com[0]] == board[winning_com[1]] &&
+      board[winning_com[1]] == board[winning_com[2]] &&
+      position_taken?(board,winning_com[0])
+    end
+  end
+
+  def full?
+    board.all? do |position|
+      position == "X" || position == "O"
+    end
+  end
+
+  def draw?
+    if full?(board) && !won?(board)
+      true
+    end
+  end
+
+  def over?
+    if won?(board) || draw?(board) || full?(board)
+      true
+    end
+  end
+
+  def winner
+    if champion = won?(board)
+      board[champion.first]
+    end
+  end
+
+  def play
+    counter = 0
+    while counter < 9
+      turn(board)
+      counter += 1
+    end
+  end
+end
