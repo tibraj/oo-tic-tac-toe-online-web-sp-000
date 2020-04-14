4fc84c1c8f6b61d3b6d746f036448ed2ffcba592
Taulant Ibraj <tibraj94@gmail.com>
2020-04-14 14:06:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,7 +38,7 @@ class TicTacToe
   end
 
   def valid_move?(index)
-    if index.between?(0, 8) && position_taken?(board, index) == false
+    if index.between?(0, 8) && position_taken?(index) == false
       return true
     else
       return false
